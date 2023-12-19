c6bdacf480d1817465ae6e1ccfddc7acd1ee2a20
melnikov <melnikov_203@mail.ru>
2023-12-19 19:20:26 +0500

    v.1.1

--- app/models/service_model.py
+++ app/models/service_model.py
@@ -4,7 +4,7 @@ from typing import Optional
 # Расширенная модель для услуги
 class Service(BaseModel):
     name: str = 'Удаление зуба'
-    price: str = '1000'
+    price: str = '1000р'
     doctor: str = 'Иванов И.И.'
     room_number: str = '304'
     description: str = 'С обезболивающим'
